d94be44e151fce4b03e6c0610c3956122c6680ab
gabrieleloy <gabriel_eloy@icloud.com>
2020-09-11 20:16:11 -0300

    ensured the existence of a valid body

--- src/presentation/routers/login-router.test.js
+++ src/presentation/routers/login-router.test.js
@@ -1,10 +1,13 @@
 class LoginRouter {
-    route (httpRequest) {
-        if(!httpRequest.body.email){
-            return {statusCode: 400}
+    route(httpRequest) {
+        if (!httpRequest || !httpRequest.body) {
+            return { statusCode: 500 }
         }
-        if(!httpRequest.body.password){
-            return {statusCode: 400}
+        
+        const { email, password } = httpRequest.body;
+
+        if (!email || !password) {
+            return { statusCode: 400 }
         }
     }
 }
@@ -31,5 +34,17 @@ describe('Login Router', () => {
         }
         const httpResponse = sut.route(httpRequest)
         expect(httpResponse.statusCode).toBe(400)
+    })
+    test('Should return 500 if there is no provided httpRequest', () => {
+        // sut stands for System Under Test
+        const sut = new LoginRouter()
+        const httpResponse = sut.route()
+        expect(httpResponse.statusCode).toBe(500)
+    })
+    test('Should return 500 if the httpRequest does not contain a body attribute', () => {
+        // sut stands for System Under Test
+        const sut = new LoginRouter()
+        const httpResponse = sut.route({})
+        expect(httpResponse.statusCode).toBe(500)
     })
 })
